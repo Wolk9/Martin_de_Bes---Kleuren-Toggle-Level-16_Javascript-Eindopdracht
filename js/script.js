5eaef44be4f3fa38059e1e8b6caaea63b5b238a6
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-05-08 13:31:44 +0200

    Voldoet aan basisRequirements plus bonusRequirements  1  en 5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// Eerst een toggle maken
 let toggleNavStatus = false;
 
 let toggleNav = function () {
   let getSidebar = document.querySelector(".nav-sidebar");
   let getSidebarUl = document.querySelector(".nav-sidebar ul");
   let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+  let getNavBtn = document.querySelector(".btn-toggle-nav");
 
   if (toggleNavStatus === false) {
     getSidebarUl.style.visibility = "visible";
     getSidebar.style.width = "272px";
-    getSidebar.style.opacity = "0.5";
+    getSidebar.style.opacity = "1";
 
     let arrayLength = getSidebarLinks.length;
 
@@ -17,10 +17,12 @@ let toggleNav = function () {
       getSidebarLinks[i].style.opacity = "1";
     }
 
-    toggleNavStatus === true;
+    toggleNavStatus = true;
   } else if (toggleNavStatus === true) {
-    getSidebar.style.width = "60px";
+    getSidebar.style.width = "0px";
     getSidebar.style.opacity = "0";
+    getNavBtn.style.visibility = "visible";
+    getNavBtn.style.zindex = "1000";
 
     let arrayLength = getSidebarLinks.length;
 
@@ -29,6 +31,12 @@ let toggleNav = function () {
     }
 
     getSidebarUl.style.visibility = "hidden";
-    toggleNavStatus === false;
+    toggleNavStatus = false;
   }
 };
+
+function colorBG(getColorBG) {
+  let getBody = document.querySelector("body");
+
+  getBody.style.backgroundColor = getColorBG;
+}
