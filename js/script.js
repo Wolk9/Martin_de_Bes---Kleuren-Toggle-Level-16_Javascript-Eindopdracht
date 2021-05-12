3a434cfe83a134e7d5b6a09ea97b25fcc98d7f5f
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-05-12 12:18:08 +0200

    Gereed voor de Bonus requirements

--- js/script.js
+++ js/script.js
@@ -1,42 +1,66 @@
-let toggleNavStatus = false;
+// function to open the hamburger Nav. When is declared in HTML with onMouseOver
 
-let toggleNav = function () {
+let openNav = function () {
   let getSidebar = document.querySelector(".nav-sidebar");
   let getSidebarUl = document.querySelector(".nav-sidebar ul");
   let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
   let getNavBtn = document.querySelector(".btn-toggle-nav");
 
-  if (toggleNavStatus === false) {
-    getSidebarUl.style.visibility = "visible";
-    getSidebar.style.width = "272px";
-    getSidebar.style.opacity = "1";
+  getSidebarUl.style.visibility = "visible";
+  getSidebar.style.width = "272px";
+  getSidebar.style.opacity = "1";
 
-    let arrayLength = getSidebarLinks.length;
+  let arrayLength = getSidebarLinks.length;
 
-    for (let i = 0; i < arrayLength; i++) {
-      getSidebarLinks[i].style.opacity = "1";
-    }
-
-    toggleNavStatus = true;
-  } else if (toggleNavStatus === true) {
-    getSidebar.style.width = "0px";
-    getSidebar.style.opacity = "0";
-    getNavBtn.style.visibility = "visible";
-    getNavBtn.style.zindex = "1000";
-
-    let arrayLength = getSidebarLinks.length;
-
-    for (let i = 0; i < arrayLength; i++) {
-      getSidebarLinks[i].style.opacity = "0";
-    }
-
-    getSidebarUl.style.visibility = "hidden";
-    toggleNavStatus = false;
+  for (let i = 0; i < arrayLength; i++) {
+    getSidebarLinks[i].style.opacity = "1";
   }
 };
 
+//function to close the hamburger Nav. This is done when a color in the menu is clicked
+
+let closeNav = function () {
+  let getSidebar = document.querySelector(".nav-sidebar");
+  let getSidebarUl = document.querySelector(".nav-sidebar ul");
+  let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+  let getNavBtn = document.querySelector(".btn-toggle-nav");
+  getSidebar.style.width = "0px";
+  getSidebar.style.opacity = "0";
+  getNavBtn.style.visibility = "visible";
+  getNavBtn.style.zindex = "1000";
+
+  let arrayLength = getSidebarLinks.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+    getSidebarLinks[i].style.opacity = "0";
+  }
+  getSidebarUl.style.visibility = "hidden";
+};
+
+//function to change the BG color of the id BG (body)
+//and to close the menu when a color is clicked
+
 function colorBG(getColorBG) {
   let getBody = document.querySelector("body");
+  let changeText = document.getElementById("p1");
 
   getBody.style.backgroundColor = getColorBG;
+  changeText.innerHTML = getColorBG;
+  changeText.style.fontWeight = 700;
+  changeText.style.fontSizeAdjust = 2;
+
+  closeNav();
+}
+
+const keyPress = document
+  .getElementById("BG")
+  .addEventListener(KeyboardEvent, colorBG);
+
+function keysDown(keyPress1) {
+  if (keyPress == "1") {
+    colorBG(grey);
+  } else if (keyPress == "2") {
+    colorBG(red);
+  }
+  1;
 }
